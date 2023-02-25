c387ad7c5020f787a443ef88ae3e20541e2d6a3a
nayyaung9 <naygit.developer@gmail.com>
2023-02-25 23:26:07 +0630

    feat: implement location module

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Location from "expo-location";
 
 import Navigation from "./src/navigation";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [location, setLocation] = useState<any>(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Navigation />
